bc46f4fa70aa5dd3d332aeb1f25149e24ee215da
shijisan <cjsantos2411638@gmail>
2025-01-01 23:16:32 +0800

    fixed padding in note/[id]

--- app/notes/[id]/page.jsx
+++ app/notes/[id]/page.jsx
@@ -41,7 +41,7 @@ export default function NoteEditor() {
 
    return (
       <>
-         <main className="pt-[10vh] md:max-w-6xl w-full m-auto md:p-0 p-4">
+         <main className="pt-[10vh!important] md:max-w-6xl w-full m-auto md:p-0 p-4">
             <button className="btn-orange my-2 inline-flex" onClick={() => router.back()}>
                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                   <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
